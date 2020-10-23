1f4a281beab662cc5c8ddff87d5d30076d5d463e
fbfb6123 <fbfb6123@gmail.com>
2020-10-23 14:05:13 +0900

    routes/web.php

--- routes/web.php
+++ routes/web.php
@@ -16,3 +16,11 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+
+Route::get('dataadd', function()
+{
+    return view('dataadd'); //登録画面表示
+});
+
+Route::any('add','dataaddcon@add'); //データ登録
