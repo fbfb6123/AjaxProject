43b321fcdc01770f711ca1ab62c254f445e57e06
fbfb6123 <fbfb6123@gmail.com>
2020-10-26 13:40:50 +0900

    sample/index.blade.php

--- resources/views/sample/index.blade.php
+++ resources/views/sample/index.blade.php
@@ -1,1 +1,18 @@
-test
+@section('content')
+    <p>{{$msg}}</p>
+    <form action="/hello" method="post">
+        <table>
+            @csrf
+            <tr><th>name: </th><td><input type="text"
+                name="name"></td></tr>
+
+            <tr><th>mail: </th><td><input type="text"
+                                          name="mail"></td></tr>
+
+            <tr><th>age: </th><td><input type="text"
+                                          name="age"></td></tr>
+            <tr><th></th><td><input type="submit"
+                value="send"></td></tr>
+        </table>
+    </form>
+@endsection
