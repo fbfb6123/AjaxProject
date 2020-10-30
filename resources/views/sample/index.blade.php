f3efc8c55b2857b463d1bd948492d16281c6c4cb
fbfb6123 <fbfb6123@gmail.com>
2020-10-30 15:31:16 +0900

    sample/index.blade.php

--- resources/views/sample/index.blade.php
+++ resources/views/sample/index.blade.php
@@ -48,58 +48,40 @@ https://remotestance.com/blog/2701/--}}
 <script>
 
     $(function() {
-        $('form').submit(function() {
-            $('form p.error_message').remove();  // エラーメッセージをクリアします。
+        $("#warn").click(function() {
+            displayWarning("「警告」が押されたので ui-state-highlight で表示します。jQuery UI のテーマによりデザインは違います。");
+            return false;
+        });
 
-            var data = {};  // POSTデータを定義します。
-            // 各要素（input[type="text"], textarea）でループします。
-            $('form input, form textarea').each(function() {
-                // POSTデータを追加します。
-                data[$(this).attr('class')] = $.trim($(this).val());
-            });
-
-            // Ajaxリクエストを投げます。
-            $.ajax({
-                url: './inquiry.php',
-                data: data,
-                dataType: 'json',
-                cache: false,
-                type: 'POST',
-                success: function(res) {
-                    if (res.is_success) {  // 入力エラーがなかった場合
-                        alert('THANKS!!');
-                    } else {  // 入力エラーがあった場合
-                        var $target = null;  // スクロールさせるターゲットを定義します。
-
-                        $.each(res.errors, function(idx, error) {
-                            // エラーが発生した入力項目を取得します。
-                            var $elem = $('form .' + error.classname);
-
-                            // 入力項目の直前に、エラーメッセージを追加します。
-                            $elem.before('<p class="error_message">' + error.message +  '</p>');
-
-                            if ($target == null || $target.offset().top > $elem.offset().top) {
-                                // スクロールのターゲットとなる入力項目を決定します。
-                                // エラーが複数存在した場合は、一番上の入力項目がターゲットになります。
-                                $target = $elem;
-                            }
-                        });
-
-                        if ($target != null) {
-                            $target.focus();  // フォーカスを当てます。
-
-                            // 入力項目を囲むdivまでスクロールさせます。
-                            $targetDiv = $target.closest('div');
-                            $('body, html').animate(
-                                {scrollTop: $targetDiv.offset().top}, 200, 'swing');
-                        }
-                    }
-                }
-            });
-
+        $("#error").click(function() {
+            displayError("「エラー」が押されたので ui-state-error で表示します。jQuery UI のテーマによりデザインは違います。");
             return false;
         });
     });
+
+    // エラー表示
+    function displayError(str) {
+        var msg = $(".messageBar");
+
+        msg
+            .text(str)
+            .addClass("ui-state-error");
+        setTimeout(function() {
+            msg.removeClass("ui-state-error", 1500);
+        }, 500);
+    }
+
+    // 警告表示
+    function displayWarning(str) {
+        var msg = $(".messageBar");
+
+        msg
+            .text(str)
+            .addClass("ui-state-highlight");
+        setTimeout(function() {
+            msg.removeClass("ui-state-highlight", 1500);
+        }, 500);
+    }
 </script>
 </html>
 
